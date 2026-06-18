7802f9fa2bf73a69379cee6db72bb636eaabb8a2
nato63 <natorono273@gmail.com>
2026-06-18 12:45:33 +0300

    deleted my test.js file

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+//calculateTax function
+function calculateTax(amount){
+   const taxRate = 0.10;
+    const tax = amount * taxRate;
+    return tax;   
+}
+//convertToUpperCase function
+function convertToUpperCase(text){
+    return text.toUpperCase();
+} 
+//findmax function
+function findMaximum(num1, num2){
+    return Math.max(num1, num2);
 
+}
+// isPalindrome
+function isPalindrome(word){
+    //
+    const cleaned = word.toLowerCase().replace(/\s/g, '');
+    const reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
+}
+//calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    const discountedPrice = originalPrice - discountAmount;    
 
-
+}
 
 
 // This is required for the test to function properly  
